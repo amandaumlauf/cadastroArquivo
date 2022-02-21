2ce85acc731ad4eb0642edf3524d30e831cf46c9
Amanda <97198491+amandaumlauf@users.noreply.github.com>
2022-02-21 09:43:11 -0300

    Update and rename AmandaUmlauf_JoãoVictorCampregher _Lista_III_Exercício_3.c to main.c

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
-/*Autor: Amanda Umlauf e João Victor Campregher
-Data:05/02/2022
-Função: Um programa em C que realiza a gravação dos dados em um arquivo texto. 
-Esse é um sistema de atendimento de uma autoescola, o usuário insere seus dados e informações referente a idade, caso seja maior de 18 anos, seus dados são salvos no arquivo e podem ser consultados pelos atendentes. 
+/*FunÃ§Ã£o: Um programa em C que realiza a gravaÃ§Ã£o dos dados em um arquivo texto. 
+Esse Ã© um sistema de atendimento de uma autoescola, o usuÃ¡rio insere seus dados e informaÃ§Ãµes referente a idade, caso seja maior de 18 anos, 
+seus dados sÃ£o salvos no arquivo e podem ser consultados pelos atendentes. 
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 
-struct cadastro //Programação C - Structs. Structs, também conhecidas como Registros, definem tipos de dados que agrupam variáveis sob um mesmo tipo de dado. A ideia de usar uma struct é permitir que, ao armazenar os dados de uma mesma entidade, isto possa ser feito com uma única variável.
+struct cadastro 
 {
 	char nome[50];
     int cpf;
@@ -28,36 +27,36 @@ FILE *pArq;//cria arquivo
 
 void cadastrar(){ //realiza o cadastro de cada um dos alunos
     
-    pArq = fopen("CadastroAlunos.txt", "a");//abre o arquivo ou cria em caso de não existir
+    pArq = fopen("CadastroAlunos.txt", "a");//abre o arquivo ou cria em caso de nÃ£o existir
 	if(pArq == NULL)//verifica se arquivo abriu corretamente
 	{
-		printf("Ocorreu um erro! O arquivo não pode ser aberto.\n");
+		printf("Ocorreu um erro! O arquivo nÃ£o pode ser aberto.\n");
 	}
 	else
 	{
-        int idade, idadeFaltante; //declaração de variaveis 
+        int idade, idadeFaltante; //declaraÃ§Ã£o de variaveis 
 
         printf("\nNome: \n");
         gets(aluno.nome);
         fflush(stdin);
 
-        printf("CPF (Apenas números, sem ponto e caracteres especiais): \n");
+        printf("CPF (Apenas nÃºmeros, sem ponto e caracteres especiais): \n");
         scanf("%d", &aluno.cpf);
         fflush(stdin);
 
-        printf("Telefone (Apenas números, sem ponto e caracteres especiais): \n");
+        printf("Telefone (Apenas nÃºmeros, sem ponto e caracteres especiais): \n");
         scanf("%d", &aluno.telefone);
         fflush(stdin);
 
-        printf("Endereço (Rua e Número): \n");
+        printf("EndereÃ§o (Rua e NÃºmero): \n");
         gets(aluno.endereco);
         fflush(stdin);
 
-        printf("Informe o CEP (Apenas números, sem ponto e caracteres especiais): \n");
+        printf("Informe o CEP (Apenas nÃºmeros, sem ponto e caracteres especiais): \n");
         gets(aluno.cep);
         fflush(stdin);
 
-        printf("Informe dia e mês de nascimento: (dd/mm) \n");
+        printf("Informe dia e mÃªs de nascimento: (dd/mm) \n");
         gets(aluno.diaNasc);
         fflush(stdin);
 
@@ -73,20 +72,20 @@ void cadastrar(){ //realiza o cadastro de cada um dos alunos
         idade = 2022 - aluno.anoNasc;
         idadeFaltante = 18 - idade;
 
-        if(idade >= 18){//verifica se idade é maior que 18 anos 
-            printf("Aluno pode se matricular e tirar habilitação.\n");
+        if(idade >= 18){//verifica se idade Ã© maior que 18 anos 
+            printf("Aluno pode se matricular e tirar habilitaÃ§Ã£o.\n");
             printf("Cadastro salvo com sucesso!\n");
-            fprintf(pArq, "Nome: %s\n", aluno.nome);//salva informações no arquivo 
+            fprintf(pArq, "Nome: %s\n", aluno.nome);//salva informaÃ§Ãµes no arquivo 
             fprintf(pArq, "CPF: %d\n", aluno.cpf);
             fprintf(pArq, "Telefone: %d\n", aluno.telefone);
-            fprintf(pArq, "Endereço: %s\n", aluno.endereco);
+            fprintf(pArq, "EndereÃ§o: %s\n", aluno.endereco);
             fprintf(pArq, "CEP: %s\n", aluno.cep);
             fprintf(pArq, "Data de Nasc: %s %d\n", aluno.diaNasc, aluno.anoNasc);
             fprintf(pArq, "Email: %s\n", aluno.email);
        
-        }else{//se não for, não há cadastro no arquivo
+        }else{//se nÃ£o for, nÃ£o hÃ¡ cadastro no arquivo
             printf(" ");
-            printf("\nAluno ainda não pode tirar habilitação, a idade mínima é 18 anos. Matrícula só será possível em %d anos\n", idadeFaltante);
+            printf("\nAluno ainda nÃ£o pode tirar habilitaÃ§Ã£o, a idade mÃ­nima Ã© 18 anos. MatrÃ­cula sÃ³ serÃ¡ possÃ­vel em %d anos\n", idadeFaltante);
             }
     }
 	fclose(pArq);
@@ -98,20 +97,20 @@ void listar(){
 	pArq = fopen("CadastroAlunos.txt", "r");
 	if(pArq == NULL)
 	{
-		printf("Ocorreu um erro! O arquivo não pode ser aberto.\n");
+		printf("Ocorreu um erro! O arquivo nÃ£o pode ser aberto.\n");
 	}else{
-   		while(!feof (pArq)){//lê o arquivo até o seu final 
-		fscanf(pArq, "Nome:%s\n", aluno.nome);//lê informações do arquivo
+   		while(!feof (pArq)){//lÃª o arquivo atÃ© o seu final 
+		fscanf(pArq, "Nome:%s\n", aluno.nome);//lÃª informaÃ§Ãµes do arquivo
         fscanf(pArq, "CPF:%d\n", &aluno.cpf);
         fscanf(pArq, "Telefone:%d\n", &aluno.telefone);
-        fscanf(pArq, "Endereço:%s\n", aluno.endereco);
+        fscanf(pArq, "EndereÃ§o:%s\n", aluno.endereco);
         fscanf(pArq, "CEP:%s\n", aluno.cep);
         fscanf(pArq, "Data de Nasc:%s %d\n", aluno.diaNasc, &aluno.anoNasc);
         fscanf(pArq, "Email:%s\n", aluno.email);
-        printf("Nome: %s\n", aluno.nome);//imprime na tela as informações existentes no arquivo
+        printf("Nome: %s\n", aluno.nome);//imprime na tela as informaÃ§Ãµes existentes no arquivo
         printf("CPF: %d\n", aluno.cpf);
         printf("Telefone: %d\n", aluno.telefone);
-        printf("Endereço: %s\n", aluno.endereco);
+        printf("EndereÃ§o: %s\n", aluno.endereco);
         printf("CEP: %s\n", aluno.cep);
         printf("Data de Nasc: %s %d\n", aluno.diaNasc, aluno.anoNasc);
         printf("Email: %s\n", aluno.email);   
@@ -124,7 +123,7 @@ void listar(){
 void excluir(){
 
     printf("Arquivo Removido\n");    
-    remove("CadastroAlunos.txt"); //exclui o arquivo e todas suas informações
+    remove("CadastroAlunos.txt"); //exclui o arquivo e todas suas informaÃ§Ãµes
     system("pause");
 }
 
@@ -135,26 +134,26 @@ void pesquisar(){
     pArq = fopen("CadastroAlunos.txt", "r");
 	if(pArq == NULL)
 	{
-		printf("Ocorreu um erro! O arquivo não pode ser aberto.\n");
+		printf("Ocorreu um erro! O arquivo nÃ£o pode ser aberto.\n");
 	}else{
         printf("Informe o cpf do cadastro que deseja consultar:");
         scanf("%d", &cpf);
 
-        while(!feof (pArq)){//lê o arquivo até o seu final 
+        while(!feof (pArq)){//lÃª o arquivo atÃ© o seu final 
             fscanf(pArq, "Nome:%s\n", aluno.nome);
             fscanf(pArq, "CPF:%d\n", &aluno.cpf);
             fscanf(pArq, "Telefone:%d\n", &aluno.telefone);
-            fscanf(pArq, "Endereço:%s\n", aluno.endereco);
+            fscanf(pArq, "EndereÃ§o:%s\n", aluno.endereco);
             fscanf(pArq, "CEP:%s\n", aluno.cep);
             fscanf(pArq, "Data de Nasc:%s %d\n", aluno.diaNasc, &aluno.anoNasc);
             fscanf(pArq, "Email:%s\n", aluno.email);
         }
         
-        if (cpf == aluno.cpf) { //compara se no arquivo há um struct contendo o cpf informado
-            printf("Nome: %s\n", aluno.nome); //imprime as informações, caso existam
+        if (cpf == aluno.cpf) { //compara se no arquivo hÃ¡ um struct contendo o cpf informado
+            printf("Nome: %s\n", aluno.nome); //imprime as informaÃ§Ãµes, caso existam
             printf("CPF: %d\n", aluno.cpf);
             printf("Telefone: %d\n", aluno.telefone);
-            printf("Endereço: %s\n", aluno.endereco);
+            printf("EndereÃ§o: %s\n", aluno.endereco);
             printf("CEP: %s\n", aluno.cep);
             printf("Data de Nasc: %s %d\n", aluno.diaNasc, aluno.anoNasc);
             printf("Email: %s\n", aluno.email); 
@@ -175,7 +174,7 @@ int main(){
         printf("__________________________________________________________\n");    
 	    printf("Sistema de gerenciamento de clientes na Autoescola Velozes\n");
         printf("__________________________________________________________\n");
-        printf("Escolha uma opção:\n");
+        printf("Escolha uma opÃ§Ã£o:\n");
         printf("1 - Cadastrar\n");
         printf("2 - Pesquisar um cadastro\n");
         printf("3 - Listar todos os cadastros\n");
@@ -184,7 +183,7 @@ int main(){
         scanf("%d", &opcao);
         fflush(stdin);
 
-        switch (opcao) {//cria um sitch, cada um possuindo uma função diferente que pode 
+        switch (opcao) {
     
             case 1:
                 cadastrar();
